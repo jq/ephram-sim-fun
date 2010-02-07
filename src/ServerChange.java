732d85399a1934e77de0e5b1adeb910ca51098c0
zzy.sysu <zzy.sysu@f7d8cb2a-13ae-11df-9262-37a038af13db>
2010-02-07 08:54:07 +0000

    random seeds replaced

--- src/ServerChange.java
+++ src/ServerChange.java
@@ -3,35 +3,28 @@ import java.util.List;
 
 import webdb.Util;
 
+public class ServerChange extends Event {
 
-public class ServerChange extends Event
-{
-	static final Date startTime = Util.toDate("19 Oct 2007 00:00");
-	static final Date endTime = Util.toDate("29 Oct 2007 00:00");
-	
-	static long interval = 1800000;	//30min
-	
-	ServerChange(long time)
-	{
-		timestamp = time;
-	}
-	
-	public void run(Cache c)
-	{
-		for(int i=0;i<c.s.length;i++)
-		{
-			int newTimeId = (int) ( Math.random()*5 );
-			int newTime = c.s[i].getAccessTimes()[newTimeId];
-			c.s[i].setAccessTime(newTime);
-		}
-	}
-	
-	static void getServerChange(List<ServerChange> ServerChangeList)
-	{		
-		for(Date ServerChangeDate = (Date) startTime.clone(); ServerChangeDate.getTime() < endTime.getTime(); ServerChangeDate.setTime(ServerChangeDate.getTime()+interval))
-		{
-			ServerChange c = new ServerChange(ServerChangeDate.getTime());
-			ServerChangeList.add(c);
-		}	
-	}
+    static final Date startTime = Util.toDate("19 Oct 2007 00:00");
+    static final Date endTime = Util.toDate("29 Oct 2007 00:00");
+    static long interval = 1800000;	//30min
+
+    ServerChange(long time) {
+        timestamp = time;
+    }
+
+    public void run(Cache c) {
+        for (int i = 0; i < c.s.length; i++) {
+            int newTimeId = (int) (Sittings.randomSeed.nextInt(5));
+            int newTime = c.s[i].getAccessTimes()[newTimeId];
+            c.s[i].setAccessTime(newTime);
+        }
+    }
+
+    static void getServerChange(List<ServerChange> ServerChangeList) {
+        for (Date ServerChangeDate = (Date) startTime.clone(); ServerChangeDate.getTime() < endTime.getTime(); ServerChangeDate.setTime(ServerChangeDate.getTime() + interval)) {
+            ServerChange c = new ServerChange(ServerChangeDate.getTime());
+            ServerChangeList.add(c);
+        }
+    }
 }
