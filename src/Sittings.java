e123255ceb2c047dcc6b3789792de61336dba959
zzy.sysu <zzy.sysu@f7d8cb2a-13ae-11df-9262-37a038af13db>
2010-02-07 07:24:31 +0000

    with data . modified the code for better data access.

---
+++ src/Sittings.java
@@ -0,0 +1,23 @@
+
+/**
+ * collect all the sittings of input file locations and names here.
+ * @author hillboy
+ */
+public class Sittings {
+    //input config
+
+    public static String sittingFolder = "datasittings/";
+    public static String dataConfig = sittingFolder + "dataConfig.txt";
+    public static String serverConfig = sittingFolder + "serverConfig.txt";
+    public static String serverProfile = sittingFolder + "serverProfile.txt";
+    public static String userConfig = sittingFolder + "userConfig.txt";
+    public static String userProfile = sittingFolder + "userProfile.txt";
+    //data
+    public static String dataFolder = "data/";
+    public static String asFile = dataFolder + "query20k-data688-10days-uniform.txt.as";
+    public static String update = dataFolder + "update.txt";
+    //output
+    public static String outputFolder = "result/";
+    public static String output = outputFolder + "output.txt";
+    public static String queryText = outputFolder + "query.txt";
+}
