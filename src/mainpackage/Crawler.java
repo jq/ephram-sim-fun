4de2b65a9391b23c7085a1a80901548a32936290
zzy.sysu <zzy.sysu@f7d8cb2a-13ae-11df-9262-37a038af13db>
2010-03-07 17:53:17 +0000

    bug fixed in Crawler

--- src/mainpackage/Crawler.java
+++ src/mainpackage/Crawler.java
@@ -5,76 +5,70 @@ import java.util.List;
 
 import webdb.Util;
 
-public class Crawler extends Event
-{
-	static final Date startTime = Util.toDate("19 Oct 2007 00:00");
-	static final Date endTime = Util.toDate("29 Oct 2007 00:00");
-	
-	static long interval = 1800000;	//30min
-	
-	static double relDeadline = 90000;//qod metric
-	static double beta = 0.1;
-	
-	static int totalCrawlTime = 0;
-	
-	Crawler(long time)
-	{
-		timestamp = time;
-	}
-	
-	public void run(Cache c)
-	{	
-		
-		/**
-		 *  problem 2B: Select a copy to refresh cache
-		 *  algo1: beta*Qos+（1-beta）*Qod
-		 */
-		for(int i=0;i<c.cacheItems.size();i++)
-		{
-			Data d = c.cacheItems.get(i);
-			//fresh data
-			if(d.cacheUnappliedUpdate==0)
-				continue;
-			
-			int selectedServerAccTime = d.src.getRecordAccessTime();
-			double serverQod = 1.0;
-			double serverQos = d.src.getRecordAccessTime()>relDeadline? 0.0:(relDeadline-d.src.getRecordAccessTime())/relDeadline;
-			double serverQosQod = serverQod*beta + serverQos*(1-beta);
-			//which replica to select;-1 means src
-			int replicaNumber = -1;
-			double[] replicasQod = new double[Data.replicaNum];
-			double[] replicasQos = new double[Data.replicaNum];
-			double[] replicasQosQod = new double[Data.replicaNum];
-			for(int j=0;j<Data.replicaNum;j++)
-			{
-				replicasQod[j] = d.getUnappliedUpdates()[j]>10? 0.0:(10-d.getUnappliedUpdates()[j])/10.0;
-				replicasQos[j] = d.replicas.get(j).getRecordAccessTime()>relDeadline? 0.0:(relDeadline-d.replicas.get(j).getRecordAccessTime())/relDeadline;		
-				replicasQosQod[j] = replicasQod[j]*beta + replicasQos[j]*(1-beta);
-				if(replicasQosQod[j]>serverQosQod)
-				{
-					serverQosQod = replicasQosQod[j];
-					replicaNumber = j;
-					//记录更新服务器的access time
-					selectedServerAccTime = d.replicas.get(replicaNumber).getRecordAccessTime();
-					totalCrawlTime += selectedServerAccTime;
-				}
-			}
-			if(replicaNumber == -1)
-			{
-				d.cacheUnappliedUpdate = 0;
+public class Crawler extends Event {
+
+    static final Date startTime = Util.toDate("19 Oct 2007 00:00");
+    static final Date endTime = Util.toDate("29 Oct 2007 00:00");
+    static long interval = 1800000;	//30min
+    static double relDeadline = 90000;//qod metric
+    static double beta = 0.1;
+    static int totalCrawlTime = 0;
+
+    Crawler(long time) {
+        timestamp = time;
+    }
+
+    public void run(Cache c) {
+
+        /**
+         *  problem 2B: Select a copy to refresh cache
+         *  algo1: beta*Qos+（1-beta）*Qod
+         */
+        for (int i = 0; i < c.cacheItems.size(); i++) {
+            Data d = c.cacheItems.get(i);
+            //fresh data
+            if (d.cacheUnappliedUpdate == 0) {
+                continue;
+            }
+
+            int selectedServerAccTime = d.src.getRecordAccessTime();
+            double serverQod = 1.0;
+            double serverQos = d.src.getRecordAccessTime() > relDeadline ? 0.0 : (relDeadline - d.src.getRecordAccessTime()) / relDeadline;
+            double serverQosQod = serverQod * beta + serverQos * (1 - beta);
+            //which replica to select;-1 means src
+            int replicaNumber = -1;
+            double[] replicasQod = new double[Data.replicaNum];
+            double[] replicasQos = new double[Data.replicaNum];
+            double[] replicasQosQod = new double[Data.replicaNum];
+            for (int j = 0; j < Data.replicaNum; j++) {
+                replicasQod[j] = d.getUnappliedUpdates()[j] > 10 ? 0.0 : (10 - d.getUnappliedUpdates()[j]) / 10.0;
+                replicasQos[j] = d.replicas.get(j).getRecordAccessTime() > relDeadline ? 0.0 : (relDeadline - d.replicas.get(j).getRecordAccessTime()) / relDeadline;
+                replicasQosQod[j] = replicasQod[j] * beta + replicasQos[j] * (1 - beta);
+                if (replicasQosQod[j] > serverQosQod) {
+                    serverQosQod = replicasQosQod[j];
+                    replicaNumber = j;
+                    //记录更新服务器的access time
+                    selectedServerAccTime = d.replicas.get(replicaNumber).getRecordAccessTime();
+
+                }
+            }
+
+            if (replicaNumber == -1) {
+                d.cacheUnappliedUpdate = 0;
+                totalCrawlTime+=selectedServerAccTime;
 //				System.out.println("Crawl from src.............................");
-			}
-			else//refresh 
-			{
-				d.cacheUnappliedUpdate = d.getUnappliedUpdates()[replicaNumber]<d.cacheUnappliedUpdate? d.getUnappliedUpdates()[replicaNumber]:d.cacheUnappliedUpdate;
+            } else//refresh
+            if (d.getUnappliedUpdates()[replicaNumber] < d.cacheUnappliedUpdate) {
+                totalCrawlTime+=selectedServerAccTime;
+                d.cacheUnappliedUpdate = d.getUnappliedUpdates()[replicaNumber];
 //				System.out.println("Crawl from other copy.............................................");
-			}		
-		}
-		
-		/**
-		 * problem 2B: Select a copy to refresh cache
-		 * algo2: select source server
-		 */
+            }
+        }
+
+        /**
+         * problem 2B: Select a copy to refresh cache
+         * algo2: select source server
+         */
 //		for(int i=0;i<c.cacheItems.size();i++)
 //		{
 //			Data d = c.cacheItems.get(i);
@@ -85,11 +79,10 @@ public class Crawler extends Event
 //			
 //			totalCrawlTime += d.src.accessTime;
 //		}
-		
-		/**
-		 * problem 2B: Select a copy to refresh cache
-		 * baseline algo3: maximize Qos
-		 */
+        /**
+         * problem 2B: Select a copy to refresh cache
+         * baseline algo3: maximize Qos
+         */
 //		for(int i=0;i<c.cacheItems.size();i++)
 //		{
 //			Data d = c.cacheItems.get(i);
@@ -124,11 +117,10 @@ public class Crawler extends Event
 //				System.out.println("Crawl from other copy.............................................");
 //			}
 //		}
-		
-		/**
-		 * problem 2B: Select a copy to refresh cache
-		 * baseline algo4: maximize Qod
-		 */
+        /**
+         * problem 2B: Select a copy to refresh cache
+         * baseline algo4: maximize Qod
+         */
 //		for(int i=0;i<c.cacheItems.size();i++)
 //		{
 //			Data d = c.cacheItems.get(i);
@@ -166,16 +158,12 @@ public class Crawler extends Event
 //				System.out.println("Crawl from other copy.............................................");
 //			}		
 //		}
-		
-	}
-	
-	static void getCrawler(List<Crawler> crawlerList)
-	{		
-		for(Date crawlDate = (Date) startTime.clone(); crawlDate.getTime() < endTime.getTime(); crawlDate.setTime(crawlDate.getTime()+interval))
-		{
-			Crawler c = new Crawler(crawlDate.getTime());
-			crawlerList.add(c);
-		}	
-	}
+    }
 
+    static void getCrawler(List<Crawler> crawlerList) {
+        for (Date crawlDate = (Date) startTime.clone(); crawlDate.getTime() < endTime.getTime(); crawlDate.setTime(crawlDate.getTime() + interval)) {
+            Crawler c = new Crawler(crawlDate.getTime());
+            crawlerList.add(c);
+        }
+    }
 }
