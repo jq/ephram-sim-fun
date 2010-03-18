81631bc93054911dfe01c09dd4d9352c9f68f210
zzy.sysu <zzy.sysu@f7d8cb2a-13ae-11df-9262-37a038af13db>
2010-03-18 14:06:01 +0000

    new Solution for Pro.3

---
+++ branches/test/src/query/SolverSimple2.java
@@ -0,0 +1,104 @@
+package query;
+
+import mainpackage.*;
+import java.util.*;
+
+/**
+ *
+ * @author LinChen
+ */
+public class SolverSimple2 extends Solver {
+
+    public static Solver getSolver() {
+        return new SolverSimple2();
+    }
+
+    public double pay(User u, int datalen, Cache c) {
+
+//		double max = 0;
+        //    if (!solution.isValid(u, datalen)) {
+        //        u.failQuery++;
+        //        return 0;
+        //    }
+        //    double max = solution.pay(u, datalen);
+        //    c.updateCache(solution.data, solution.source);
+//        System.out.println(" DataPrice~~~~~~~~~~~" + solution.getDataPrice());
+        //    return max;
+        double max = 0;
+        int maxNum = 0;
+        boolean ifmaxInitial = false;
+        for (int i = 0; i < 1 + c.s.length; i++) {
+            if (solutions.get(i).isValid2(u, datalen)) {
+                if (!ifmaxInitial) {
+                    ifmaxInitial = true;
+                    maxNum = i;
+                    max = solutions.get(i).tryPay(u, datalen);
+                } else if (solutions.get(i).tryPay(u, datalen) > max) {
+                    maxNum = i;
+                    max = solutions.get(i).tryPay(u, datalen);
+                }
+            }
+        }
+        if (!ifmaxInitial) {
+            for (int i = 0; i < solutions.get(0).data.length; i++) {
+                if (solutions.get(0).source[i] != -2) {
+                    Cache.notinCacheCount++;
+                }
+            }
+            solutions.get(0).isValid(u, datalen);
+            u.failQuery++;
+            return 0;
+        }
+        for (int i = 0; i < solutions.get(maxNum).data.length; i++) {
+            if (solutions.get(maxNum).source[i] != -2) {
+                Cache.notinCacheCount++;
+            }
+        }
+        max = solutions.get(maxNum).pay(u, datalen);
+        c.updateCache(solutions.get(maxNum).data, solutions.get(maxNum).source);
+        return max;
+    }
+    public ArrayList<SolutionSimple2> solutions;
+
+    public void solve(Data[] data, Cache c) {
+        solutions = new ArrayList<SolutionSimple2>(1 + c.s.length);
+        int[] allServerAccessTimes = new int[1 + c.s.length];
+        allServerAccessTimes[0] = Cache.cacheAccessTime;
+        for (int i = 1; i < 1 + c.s.length; i++) {
+            allServerAccessTimes[i] = c.s[i - 1].getAccessTime();
+        }
+        for (int j = 0; j < 1 + c.s.length; j++) {
+            SolutionSimple2 solution = new SolutionSimple2(data.length);
+            solution.data = data;
+            for (int i = 0; i < data.length; i++) {
+                data[i].access();
+                if (c.inCache(data[i])) {
+                    // get from cache
+                    solution.source[i] = -2;
+                } else {
+                    // get from src
+                    //Cache.notinCacheCount++;
+                    //solution.source[i] = -1;
+                    //get from src or replicas
+                    if (data[i].src.getAccessTime() <= allServerAccessTimes[j]) {
+                        solution.source[i] = -1;
+                    } else {
+                        int getFromReplicasNum = 0;
+                        for (int k = 1; k < 4; k++) {
+                            if (data[i].replicas.get(k).getAccessTime() <= allServerAccessTimes[j] && data[i].unappliedUpdates[k] < data[i].unappliedUpdates[getFromReplicasNum]) {
+                                getFromReplicasNum = k;
+                            }
+                        }
+                        if (getFromReplicasNum == 0 && data[i].unappliedUpdates[getFromReplicasNum] > allServerAccessTimes[j]) {
+                            solution.source[i] = -1;
+                        } else {
+                            solution.source[i] = getFromReplicasNum;
+                        }
+                    }
+                }
+            }
+            solution.getFreshAndTime();
+            solutions.add(solution);
+        }
+    }
+}
