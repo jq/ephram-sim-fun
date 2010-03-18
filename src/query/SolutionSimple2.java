55b077fbbaa98a694e6964cfa7d71bff36deb292
zzy.sysu <zzy.sysu@f7d8cb2a-13ae-11df-9262-37a038af13db>
2010-03-18 14:06:01 +0000

    new Solution for Pro.3

---
+++ src/query/SolutionSimple2.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package query;
+import mainpackage.*;
+
+import java.util.*;
+/**
+ *
+ * @author LinChen
+ */
+public class SolutionSimple2 extends Solution {
+
+    public Data[] data;
+    public int[] source;
+
+    public SolutionSimple2(int len) {
+        source = new int[len];
+    }
+
+    public void getFreshAndTime() {
+
+        for (int i = 0; i < data.length; i++) {
+            //update time
+
+            if (source[i] == -2) {
+                if (Cache.cacheAccessTime > time) {
+                    time = Cache.cacheAccessTime;
+                }
+            }
+            if (source[i] == -1) {
+                if (data[i].src.getRecordAccessTime() > time) {
+                    time = data[i].src.getRecordAccessTime();
+                }
+            }
+            if (source[i] >= 0) {
+                if (data[i].replicas.get(source[i]).getRecordAccessTime() > time) {
+                    time = data[i].replicas.get(source[i]).getRecordAccessTime();
+                }
+            }
+            //update refresh
+            if (source[i] == -2 && data[i].cacheUnappliedUpdate == 0) {
+                fresh++;
+            }
+            if (source[i] == -1) {
+                fresh++;
+            }
+            if (source[i] >= 0 && data[i].unappliedUpdates[source[i]] == 0) {
+                fresh++;
+            }
+        }
+
+    }
+    public boolean isValid2(User u, float datalen) {
+        float stale = (datalen - fresh) / datalen;
+        boolean cool = true;
+        if (u.getQod_linearPositive(stale) == 0) {
+            //Statistics.QoDrejection++;
+            cool = false;
+        }
+        if (u.getQos_linearPositive(time) == 0) {
+            //Statistics.QoSrejection++;
+            cool = false;
+        }
+        return cool;
+    }
+}
